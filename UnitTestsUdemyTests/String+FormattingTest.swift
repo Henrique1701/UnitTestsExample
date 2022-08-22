7ebb535254f05131b9b3d0ffa51deb7bd32544ad
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2022-08-22 17:05:57 -0300

    Adiciona teste para a função isValidEmailAddress

--- UnitTestsUdemyTests/String+FormattingTest.swift
+++ UnitTestsUdemyTests/String+FormattingTest.swift
@@ -9,6 +9,14 @@ import XCTest
 @testable import UnitTestsUdemy
 
 class String_FormattingTest: XCTestCase {
+    func testIsValidEmailAddress() {
+        let validEmail = "user@gmail.com"
+        let notValidEmail = "user"
+        
+        XCTAssertTrue(validEmail.isValidEmailAddress())
+        XCTAssertFalse(notValidEmail.isValidEmailAddress())
+    }
+    
     func testIsBigText() {
         isBigText(for: "O meu nome é Henrique", expectedResult: true, message: "O texto deve ter mais de 5 palavras para ser considerado grande")
         isBigText(for: "Olá", expectedResult: false, message: "O texto deve ter pelo menos um palavra e menos de 5 palavras")
