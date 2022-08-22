5785b9bb4bcc5075c5e93293c59e3da06a57e7dc
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2022-08-22 16:45:18 -0300

    Adiciona target de testes unitários

---
+++ UnitTestsUdemyTests/String+FormattingTest.swift
@@ -0,0 +1,13 @@
+//
+//  String+FormattingTest.swift
+//  UnitTestsUdemyTests
+//
+//  Created by José Henrique Fernandes Silva on 22/08/22.
+//
+
+import XCTest
+@testable import UnitTestsUdemy
+
+class String_FormattingTest: XCTestCase {
+    
+}
